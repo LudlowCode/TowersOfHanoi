804f7df21edaaa49cbbdffef2f4c9451cf7a8f9c
LudlowCode <90901975+LudlowCode@users.noreply.github.com>
2025-03-26 08:44:30 +0000

    print format hack

--- Program.cs
+++ Program.cs
@@ -45,12 +45,21 @@ class Tower
       return pole[pointer];
     }
   }
+  static void PrintCharacterRepeatedly(char c, int num){
+    for (int i = 0; i<num; i++){
+      Console.Write(c);
+    }
+  }
   public void PrintTower(){
     Console.WriteLine("----------------------------------");
     Console.WriteLine("Tower contents:\n");
     int i = pointer;
     while (i > 0){
-      Console.WriteLine(i);
+      PrintCharacterRepeatedly(' ', i-1);
+      PrintCharacterRepeatedly('#', pointer-i+1);
+      Console.Write('|');
+      PrintCharacterRepeatedly('#', pointer-i+1);
+      Console.Write('\n');
       i--;
     }
     Console.WriteLine("----------------------------------");
@@ -63,8 +72,10 @@ class Program{
   static void Main(string[] args)
   {
     //Calling the constructor of the Tower class to make a new Tower object
+    
     Tower tower1 = new Tower(10);
     tower1.Push(3);
+    
     tower1.Push(2);
     tower1.Push(1);
     tower1.PrintTower();
